93a008e50ddbaffda37d464dcd25026deec72660
Murat <muratseit@gmail.com>
2017-04-17 10:08:37 +0600

    creating

---
+++ vk1/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace WindowsFormsApplication4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.History = new System.Windows.Forms.ListBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(105, 97);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(163, 28);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "+77774000488";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.textBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft NeoGothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(106, 156);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '•';
+            this.textBox2.Size = new System.Drawing.Size(163, 34);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.Text = "mstxxxd";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(106, 213);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 57);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Log in";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // listBox
+            // 
+            this.listBox.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.listBox.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.listBox.FormattingEnabled = true;
+            this.listBox.ItemHeight = 21;
+            this.listBox.Location = new System.Drawing.Point(360, 12);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(318, 445);
+            this.listBox.TabIndex = 9;
+            this.listBox.SelectedIndexChanged += new System.EventHandler(this.listBox_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.ForeColor = System.Drawing.Color.Linen;
+            this.button2.Location = new System.Drawing.Point(684, 415);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 42);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "quit";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.quit_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 344);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(342, 50);
+            this.textBox3.TabIndex = 11;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.ForeColor = System.Drawing.Color.AntiqueWhite;
+            this.button3.Location = new System.Drawing.Point(12, 397);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(342, 60);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Send";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.buttonSending_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button4.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.button4.Location = new System.Drawing.Point(684, 377);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 32);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "hide chat";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button5.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button5.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.button5.Location = new System.Drawing.Point(684, 344);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 32);
+            this.button5.TabIndex = 15;
+            this.button5.Text = "show chat";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // History
+            // 
+            this.History.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.History.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.History.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.History.FormattingEnabled = true;
+            this.History.HorizontalScrollbar = true;
+            this.History.ItemHeight = 20;
+            this.History.Location = new System.Drawing.Point(12, 74);
+            this.History.Name = "History";
+            this.History.Size = new System.Drawing.Size(342, 264);
+            this.History.TabIndex = 16;
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(19, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 17);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Telephone ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.ForeColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(19, 156);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 17);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Password";
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button6.ForeColor = System.Drawing.Color.Honeydew;
+            this.button6.Location = new System.Drawing.Point(12, 12);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(342, 60);
+            this.button6.TabIndex = 19;
+            this.button6.Text = " ⟲ ";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(361, 501);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.History);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "VK Messenger";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ListBox History;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ vk1/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Runtime.Remoting.Messaging;
+using System.Windows.Forms;
+using VkNet;
+using VkNet.Enums.Filters;
+using VkNet.Model;
+using VkNet.Model.RequestParams;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        VkApi vkApi = new VkApi();
+        long? thisUserID = 0;
+        long UserId = 0;
+        string thisUserName = "";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            textBox3.Visible = false;
+            History.Visible = false;
+            button3.Visible = false;
+            listBox.Visible = false;
+            button6.Visible = false;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        //public void GetOnlineFriends()
+        //{
+        //    var online = vkApi.Friends.GetOnlineEx(new FriendsGetOnlineParams{
+
+        //        UserId = long.Parse(UserId.ToString())
+
+
+        //    });
+
+        //    foreach (var friend in online.Online)
+        //    {
+        //        //listBox.Items.Add(friend.FirstName + " " + friend.LastName);
+        //    }
+        //}
+
+        public void GetFriends()
+        {
+            var users = vkApi.Friends.Get(long.Parse(thisUserID.ToString()), ProfileFields.FirstName | ProfileFields.LastName);
+
+            foreach (var friend in users)
+            {
+                listBox.Items.Add(friend.FirstName + " " + friend.LastName + " | " + friend.Id);
+            }
+        }
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            int appID = 5982087;
+            string email = textBox1.Text;
+            string password = textBox2.Text;
+            Settings settings = Settings.All;
+            try
+            {
+                listBox.Items.Clear();
+                vkApi.Authorize(new ApiAuthParams
+                {
+                    ApplicationId = (ulong)appID,
+                    Login = email,
+                    Password = password,
+                    Settings = settings
+                });
+
+                thisUserID = vkApi.UserId;
+                thisUserName = vkApi.Users.Get(long.Parse(thisUserID.ToString())).FirstName;
+                GetFriends();
+                //GetOnlineFriends();
+                this.timer.Enabled = true;
+                textBox1.Visible = false;
+                textBox2.Visible = false;
+                button1.Visible = false;
+                textBox3.Visible = true;
+                History.Visible = true;
+                button3.Visible = true;
+                listBox.Visible = true;
+                label1.Visible = false;
+                label2.Visible = false;
+                button6.Visible = true;
+
+            }
+            catch
+            {
+                MessageBox.Show("Неверный логин или пароль. Либо проверьте интернет-соединение.");
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void quit_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+
+
+        private void buttonSending_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox3.Text != "")
+                {
+                    // var send =
+                    vkApi.Messages.Send(UserId, false, textBox3.Text);
+
+                    textBox3.Text = "";
+                }
+            }
+            catch
+            {
+                MessageBox.Show("no msg");
+            }
+        }
+
+        private void listBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+
+                UserId = long.Parse(listBox.Text.Substring(listBox.Text.LastIndexOf(' ')));
+
+            }
+            catch
+            {
+                MessageBox.Show("");
+            }
+            GetHistory();
+        }
+
+        void Hiding()
+        {
+            button3.Visible = false;
+            textBox3.Visible = false;
+            History.Visible = false;
+            listBox.Visible = false;
+            button6.Visible = false;
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Hiding();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            button3.Visible = true;
+            textBox3.Visible = true;
+            History.Visible = true;
+            listBox.Visible = true;
+            button6.Visible = true;
+        }
+
+
+        void GetHistory()
+        {
+            History.Items.Clear();
+            var getHistory = vkApi.Messages.GetHistory(new MessagesGetHistoryParams
+            {
+                Count = 200,
+                UserId = long.Parse(UserId.ToString()),
+            });
+
+
+            foreach (var message in getHistory.Messages)
+            {
+                if (message.FromId == thisUserID)
+                {
+                    History.Items.Add(thisUserName + ": " + message.Body);
+
+                }
+                if (message.FromId == UserId)
+                {
+                    History.Items.Add(listBox.Text.Substring(0, listBox.Text.IndexOf(' ')) + ": " + message.Body);
+
+                }
+            }
+
+
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (this.Width >= 590) this.timer.Enabled = false;
+            else this.Width += 5;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                GetHistory();
+            }
+            catch
+            {
+                MessageBox.Show("There are no object for update");
+            }
+        }
+
+        //private void button6_Click(object sender, EventArgs e)
+        //{
+        //    this.timer.Enabled = true;
+        //}
+    }
+}
+
